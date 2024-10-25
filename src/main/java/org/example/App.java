d830a971867e0807d990b58527afb9d1280b10c2
Hitesh kumar <123413043+hitesh-munwani@users.noreply.github.com>
2024-10-25 14:42:38 +0500

    Initial Hibernate configuration setup

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory session = cfg.buildSessionFactory();
+        System.out.println(session.isClosed());
+
+
+    }
+}
