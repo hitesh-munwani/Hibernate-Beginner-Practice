0ba534f77541c49959d948b222600ed872163fbb
Hitesh kumar <123413043+hitesh-munwani@users.noreply.github.com>
2024-10-26 15:22:55 +0500

    Implemented functionality to add images to the database using Hibernate and created Address class.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,13 +5,18 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.stream.Stream;
+
 /**
  * Main class to demonstrate a simple Hibernate application.
  */
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) throws IOException {
         // Print a greeting message to the console
         System.out.println("Hello World!");
 
@@ -29,6 +34,18 @@ public class App
         st.setCity("Karachi"); // Set city for the student
         System.out.println(st.toString()); // Print the student object to console
 
+        // Create a new obj of Address Class
+        Address ad = new Address();
+        ad.setStreet("street1");
+        ad.setCity("Karachi");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(11.25);
+        FileInputStream fis = new FileInputStream("src/main/java/org/example/pixel.jpg");
+        byte[] data = fis.readAllBytes();
+        ad.setImage(data);
+        System.out.println(ad.toString());
+
         // Open a new session
         Session session = factory.openSession();
 
@@ -37,6 +54,7 @@ public class App
 
         // Save the student object to the database
         session.save(st);
+        session.save(ad);
 
         // Commit the transaction to save the changes
         tx.commit();
